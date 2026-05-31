4ce62d4a6152ce108bd4dff298c170b801efc0d8
Raffael Eloi <raffaeleloi121@gmail.com>
2026-05-31 10:25:43 -0300

    add terraform lab1 examples

---
+++ lab1/main.tf
@@ -0,0 +1,12 @@
+// 1. block type
+// 2. resource type
+// 3. object reference
+
+# Terraform init -> Initialize the working director
+
+resource "random_string" "suffix" {
+  // attributes here
+  length  = 6
+  upper   = false
+  special = false
+}
