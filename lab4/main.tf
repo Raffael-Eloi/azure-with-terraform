57147eb3edb58fc01b92eb17554530a16ba9503d
Raffael Eloi <raffaeleloi121@gmail.com>
2026-06-16 10:35:13 -0300

    add role assigment to azure key vault

--- lab4/main.tf
+++ lab4/main.tf
@@ -18,3 +18,9 @@ resource "azurerm_key_vault" "main" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 }
+
+resource "azurerm_role_assignment" "terraform_user" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
