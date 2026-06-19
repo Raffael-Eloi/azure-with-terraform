8726ca759c8e19a7e097d5115ed0246f9373c82d
Raffael Eloi <raffaeleloi121@gmail.com>
2026-06-19 15:57:43 -0300

    add virtual machine

--- lab6/main.tf
+++ lab6/main.tf
@@ -29,3 +29,37 @@ resource "azurerm_network_interface" "vm1" {
     public_ip_address_id          = azurerm_public_ip.vm1.id
   }
 }
+
+resource "tls_private_key" "vm1" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1${var.application_name}${var.environment_name}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.vm1.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.vm1.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
