1e5903757536307da2858c5f63893191f814bb30
Raffael Eloi <raffaeleloi121@gmail.com>
2026-06-21 13:04:22 -0300

    provision ssh private and public key to azure key vault secret

--- lab6/main.tf
+++ lab6/main.tf
@@ -34,16 +34,35 @@ resource "tls_private_key" "vm1" {
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key" {
-  content         = tls_private_key.vm1.private_key_pem
-  filename        = pathexpand("~/.ssh/vm1")
-  file_permission = "0600"
+// Replaced with azure key vault
+# resource "local_file" "private_key" {
+#   content         = tls_private_key.vm1.private_key_pem
+#   filename        = pathexpand("~/.ssh/vm1")
+#   file_permission = "0600"
+# }
+
+// Replaced with azure key vault
+# resource "local_file" "public_key" {
+#   content         = tls_private_key.vm1.public_key_openssh
+#   filename        = pathexpand("~/.ssh/vm1.pub")
+#   file_permission = "0600"
+# }
+
+data "azurerm_key_vault" "main" {
+  name                = "kv-devops-prod-9bbu53"
+  resource_group_name = "rg-devops-prod"
 }
 
-resource "local_file" "public_key" {
-  content         = tls_private_key.vm1.public_key_openssh
-  filename        = pathexpand("~/.ssh/vm1.pub")
-  file_permission = "0600"
+resource "azurerm_key_vault_secret" "vm1_ssh_private" {
+  name         = "vm1-ssh-private"
+  value        = tls_private_key.vm1.private_key_openssh
+  key_vault_id = data.azurerm_key_vault.main.id
+}
+
+resource "azurerm_key_vault_secret" "vm1_ssh_public" {
+  name         = "vm1-ssh-public"
+  value        = tls_private_key.vm1.public_key_openssh
+  key_vault_id = data.azurerm_key_vault.main.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
