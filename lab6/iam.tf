987b821635a26214a97dfa582a3c068808b9dab3
Raffael Eloi <raffaeleloi121@gmail.com>
2026-06-21 19:12:59 -0300

    manage users in a better way using terraform

--- lab6/iam.tf
+++ lab6/iam.tf
@@ -6,11 +6,34 @@ resource "azuread_group" "remote_access_users" {
   security_enabled = true
 }
 
-resource "azuread_group_member" "raffa_remote_access_membership" {
+# resource "azuread_group_member" "remote_access_users_membership" {
+#   count = length(var.remote_access_users)
+
+#   group_object_id  = azuread_group.remote_access_users.object_id
+#   member_object_id = data.azuread_user.remove_access_users[count.index].object_id
+# }
+
+# data "azuread_user" "remove_access_users" {
+#   count = length(var.remote_access_users)
+
+#   user_principal_name = var.remote_access_users[count.index]
+# }
+
+// Using foreach
+// It's a better approach because the resource is not tied to the index anymore, but the email itself
+
+locals {
+  remove_access_users_map = { for index, element in var.remote_access_users : element => index }
+}
+resource "azuread_group_member" "remote_access_users_membership" {
+  for_each = local.remove_access_users_map
+
   group_object_id  = azuread_group.remote_access_users.object_id
-  member_object_id = data.azuread_user.raffa.object_id
+  member_object_id = data.azuread_user.remove_access_users[each.key].object_id
 }
 
-data "azuread_user" "raffa" {
-  user_principal_name = "myprincipalname"
+data "azuread_user" "remove_access_users" {
+  for_each = local.remove_access_users_map
+
+  user_principal_name = each.key
 }
--- lab6/terraform.tfvars
+++ lab6/terraform.tfvars
@@ -1,2 +1,6 @@
 application_name = "linuxvm"
 primary_location = "westus3"
+
+remote_access_users = [
+  "my_principal_email"
+]
