e313d6a8c98bff247633aefa67951a5aebf52e76
Raffael Eloi <raffaeleloi121@gmail.com>
2026-06-21 18:57:14 -0300

    add user data source

--- lab6/iam.tf
+++ lab6/iam.tf
@@ -8,5 +8,9 @@ resource "azuread_group" "remote_access_users" {
 
 resource "azuread_group_member" "raffa_remote_access_membership" {
   group_object_id  = azuread_group.remote_access_users.object_id
-  member_object_id = data.azuread_client_config.current.object_id
+  member_object_id = data.azuread_user.raffa.object_id
+}
+
+data "azuread_user" "raffa" {
+  user_principal_name = "myprincipalname"
 }
