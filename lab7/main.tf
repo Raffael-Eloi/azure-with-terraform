6ee7fabd2c3b2fd02fdddc0a5155f236d013d769
Raffael Eloi <raffaeleloi121@gmail.com>
2026-06-21 21:01:02 -0300

    provision public ip using azapi

--- lab7/main.tf
+++ lab7/main.tf
@@ -7,3 +7,20 @@ resource "azapi_resource" "rg" {
 }
 
 data "azapi_client_config" "current" {}
+
+resource "azapi_resource" "vm_pip" {
+  type      = "Microsoft.Network/publicIPAddresses@2024-05-01"
+  name      = "pip-${var.application_name}-${var.environment_name}"
+  location  = azapi_resource.rg.location
+  parent_id = azapi_resource.rg.id
+
+  body = {
+    properties = {
+      publicIPAllocationMethod = " Static"
+      publicIPAddressVersion   = "IPv4"
+    }
+    sku = {
+      name = "Standard"
+    }
+  }
+}
